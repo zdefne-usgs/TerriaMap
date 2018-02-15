9105fc03e6554224b204500f84b5abf4227c14d4
Zafer <zdefne-usgs@users.noreply.github.com>
2018-02-14 22:41:30 -0500

    Update UserInterface.jsx

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -23,7 +23,7 @@ export default function UserInterface(props) {
     return (
         <StandardUserInterface {... props} version={version}>
             <Menu>
-                <MenuItem caption="About" href="https://www.usgs.gov" key="about-link"/>
+                <MenuItem caption="About" href="http://arcg.is/1He0Tz" key="about-link"/>
             </Menu>
             <Nav>
                 <MeasureTool terria={props.viewState.terria} key="measure-tool"/>
